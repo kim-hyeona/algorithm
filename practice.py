a53086ba6f84cc5d2cc665bf91b8c86f4c8495fe
kim-hyeona <kimha9@naver.com>
2023-02-15 22:51:53 +0900

    python basic 1

---
+++ practice.py
@@ -0,0 +1,14 @@
+print(3.14)
+print(1000)
+print(5+3)
+print('풍선')
+print("스껄")
+print("스껄"*9)
+
+# 이것이 파이썬 주석인가
+
+print(5>10) #false
+print(5<10) #True
+
+print(not True) #false
+print(not False) #False
