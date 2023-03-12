332c83b624ad06163213a22f496b95987c80d039
kim-hyeona <kimha9@naver.com>
2023-03-12 22:48:41 +0900

    python사전까지

--- practice.py
+++ practice.py
@@ -261,3 +261,133 @@ my_str = my_str[:my_str.index('.')] #규칙2
 password = my_str[:3] + str(len(my_str)) + str(my_str.count('e')) + "!"
 
 print('{0}의 비밀번호는 {1} 입니다'.format(site, password))
+
+
+print('-----------------------리스트-------------------------');
+
+#지하철 칸별로 10명 20명 30명이 있다고 가정
+
+# subway1 = 10
+# subway2 = 20
+# subway3 = 30
+
+subway = [10,20,30]
+print(subway)
+
+subway = ["유재석","조세호","박명수"]
+print(subway)
+
+# 조세호씨가 몇 번째 칸에 타고 있는가?
+
+print(subway.index('조세호'))
+
+
+# 하하씨가 다음 정류장에서 다음 칸에 탐
+subway.append('하하')
+print(subway)
+
+# 정형돈씨를 유재석 / 조세호 사이에 태워봄
+
+subway.insert(1,"정형돈")
+print(subway)
+
+# 지하철에 있는 사람을 한 명씩 뒤에서 꺼냄
+print(subway.pop())
+print(subway)
+
+print(subway.pop())
+print(subway)
+
+print(subway.pop())
+print(subway)
+
+
+# 같은 이름의 사람이 몇명 있는 지 확인하기
+
+subway.append("유재석") #같은 이름이 없으니 유재석 한번 더 추가
+print(subway) # ['유재석', '정형돈', '유재석']
+print(subway.count('유재석')) # 2
+
+
+# 정렬도 가능
+
+num_list = [5,2,4,3,1]
+num_list.sort()
+
+print(num_list)
+
+# 순서 뒤집기
+
+num_list.reverse()
+print(num_list)
+
+# 모두 지우기
+
+num_list.clear()
+print(num_list)
+
+
+# 다양한 자료형을 함께 사용
+
+num_list = [5,2,4,3,1]
+mix_list = ["조세호",20,True]
+
+# print(mix_list)
+
+# 리스트 확장
+num_list.extend(mix_list)
+print(num_list)
+
+
+
+print('-----------------------사전-------------------------');
+
+cabinet = {3:'유재석',100:'김태호'}
+
+print(cabinet[3])
+print(cabinet[100])
+
+print(cabinet.get(3))
+
+print(cabinet.get(5))
+print(cabinet.get(5,"사용가능"))
+
+
+print(3 in cabinet) #True
+print(5 in cabinet) #False
+
+
+cabinet2 = {'a-3':'유재석','a-100':'김태호'}
+print(cabinet2['a-3'])
+print(cabinet2['a-100'])
+
+
+
+# 새 캐비넷 주인
+print(cabinet2)
+cabinet2["c-20"] = '조세호'
+
+print(cabinet2['c-20'])
+
+
+# 주인 빠진 캐비넷
+
+del cabinet2['a-3']
+print(cabinet2)
+
+
+# key들만 출력
+
+print(cabinet2.keys())
+
+# value들만 출력
+
+print(cabinet2.values())
+
+# key, value 쌍으로 출력
+print(cabinet.items())
+
+# 캐비넷 폐점
+
+cabinet2.clear()
+print(cabinet2)
