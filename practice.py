354c4017a36f782ce4feffd9b268e888741608b2
kim-hyeona <kimha9@naver.com>
2023-03-16 22:10:33 +0900

    set

--- practice.py
+++ practice.py
@@ -391,3 +391,87 @@ print(cabinet.items())
 
 cabinet2.clear()
 print(cabinet2)
+
+print('-----------------------튜플-------------------------');
+
+menu = ('돈까스','치즈까스')
+
+print(menu[0])
+print(menu[1])
+
+#튜플은 고정된 값에서 사용하는 것, 
+
+# name = "김종국"
+# age="99"
+# hobby="코딩"
+# print(name,age,hobby)
+
+#=>
+(name , age , hobby) = ('김종국','99','코딩')
+print(name,age,hobby)
+
+print('-----------------------세트-------------------------');
+# 집합(set)
+# 중복 안됨, 순서 없음
+my_set = {1,2,3,4,5,5}
+print(my_set) # 1,2,3,4,5
+
+java = {'유재석','김태호','양세형'}
+python = set(['유재석','박명수'])
+# 교집합 (java와 python을 모두 할 수 있는 개발자) 출력
+
+print(java & python)
+print(java.intersection(python)) #유재석 출력
+
+# 합집합 (java를 할 수 있거나 python을 할 수 있는 개발자) 출력
+print(java | python)
+print(java.union(python))
+
+# 차집합 (java는 할 수 있지만 python은 할 줄 모르는 개발자) 출력
+print(java - python)
+print(java.difference(python))
+
+
+# 파이썬을 할 줄 아는 개발자가 생김
+python.add("김태호")
+print(python)
+
+# 자바를 잊어서 자바개발자에서 제외됨
+java.remove('김태호')
+print(java)
+
+print('-----------------------자료구조의 변경-------------------------');
+#커피숍
+menu = {'커피','우유','주스'}
+print(menu, type(menu)) #{'커피', '주스', '우유'} <class 'set'>
+
+menu = list(menu)
+print(menu, type(menu)) #['우유', '주스', '커피'] <class 'list'>
+
+menu = tuple(menu)
+print(menu, type(menu)) #('커피', '우유', '주스') <class 'tuple'>
+
+menu = set(menu)
+print(menu, type(menu)) #{'커피', '우유', '주스'} <class 'set'>
+
+
+print('-----------------------퀴즈-------------------------');
+
+# list = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+# print(list)
+# shuffle(list)
+# print(list)
+
+from random import *
+user = range(1,21) #1부터 20까지 숫자생성
+# print(type(user))
+user = list(user)
+# print(type(user))
+
+print(user)
+shuffle(user)
+print(user)
+
+winners = sample(user, 4)
+print('치킨 당첨자: {0}'.format(winners[0]))
+print('커피 당첨자: {0}'.format(winners[1:]))
