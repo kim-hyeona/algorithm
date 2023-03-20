206af693f3b44794131ac10bf479df6396a48bb1
kim-hyeona <kimha9@naver.com>
2023-03-20 21:55:20 +0900

    if,for,while

--- practice.py
+++ practice.py
@@ -475,3 +475,75 @@ print(user)
 winners = sample(user, 4)
 print('치킨 당첨자: {0}'.format(winners[0]))
 print('커피 당첨자: {0}'.format(winners[1:]))
+
+
+
+print('-----------------------if문-------------------------');
+
+# weather = input('오늘 날씨는 어때요?')
+# # if 조건:
+# #     실행 명령문
+
+# if weather == "비":
+#     print('우산을 챙기세요')
+# elif weather == "미세먼지":
+#     print('마스크를 챙기세요')
+# else:
+#     print('준비물 필요없어요.')
+
+
+# temp = int(input('기온은 어때요?'))
+# if 30 <= temp:
+#     print('너무 더워요. 나가지 마세요')
+# elif 10 <= temp and temp < 30:
+#     print('괜찮은 날씨에요')
+# elif 0 <= temp and temp < 10:
+#     print('외투를 챙기세요')
+# else:
+#     print('너무 추워요. 나가지 마세요')
+
+
+print('-----------------------for문-------------------------');
+
+# print('대기번호 : 1')
+# print('대기번호 : 2')
+# print('대기번호 : 3')
+# print('대기번호 : 4')
+
+for waiting_no in [0,1,2,3,4]:
+    print('대기번호: {0}'.format(waiting_no))
+
+# randrange()
+for waiting_no in range(1, 6): # 1, 2, 3, 4, 5
+    print('대기번호: {0}'.format(waiting_no))
+
+
+starbucks = ['아이언맨','토르','아이엠 그루트']
+for customer in starbucks:
+    print("{0},커피가 준비되었습니다".format(customer))
+
+
+print('-----------------------while문-------------------------');
+
+# customer = '토르'
+# index = 5
+# while index >= 1:
+#     print("{0}, 커피가 준비되었습니다. {1}번 남았어요".format(customer,index))
+#     index -= 1
+#     if index == 0:
+#         print('커피는 폐기처분되었습니다')
+
+
+# customer = "아이언맨"
+# index = 1
+# while True:
+#     print("{0}, 커피가 준비되었습니다. 호출{1}회".format(customer, index))
+#     index += 1
+ #무한루프 코드 컨트롤 c 누르면 종료
+
+customer = "토르"
+person = "UnKnown"
+
+while person != customer :
+    print("{0},커피가 준비 되었습니다.".format(customer))
+    person = input('이름이 어떻게 되세요?')
